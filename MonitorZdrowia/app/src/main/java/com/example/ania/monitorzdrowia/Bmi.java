c7b318cafccee3251a733769346ebd48ebce282f
Anna <anusia1199@o2.pl>
2017-06-06 01:25:18 +0200

    Weight advice

--- MonitorZdrowia/app/src/main/java/com/example/ania/monitorzdrowia/Bmi.java
+++ MonitorZdrowia/app/src/main/java/com/example/ania/monitorzdrowia/Bmi.java
@@ -13,12 +13,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
 public class Bmi extends AppCompatActivity {
     TextView bmiText;
+    TextView advice;
 Context c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +30,7 @@ Context c;
         setSupportActionBar(toolbar);
        c= getApplicationContext();
          bmiText=(TextView)findViewById(R.id.textView14);
-
-
+advice=(TextView) findViewById(R.id.textView16);
        /* setContentView(R.layout.activity_bmi);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
@@ -83,6 +84,24 @@ Context c;
         else{
             bmiText.setText("brak danych");
         }
+
+        if(bmi<20){
+            advice.setText("Masz niedowagę. \nSkontaktuj się z lekarzem.");
+        }
+        else if(bmi<25){
+            advice.setText("Brawo! Twoja waga jest w normie. \nCwicz regularnie i trzymaj prawidłową dietę.");
+        }
+        else if(bmi<30){
+            advice.setText("Masz nadwagę. \nNie załamuj się. Skontaktuj się z dietetykiem, odstaw słodycze. \nCwicz regularnie i trzymaj prawidłową dietę. ");
+        }
+
+        else if(bmi>40){
+            advice.setText("Jesteś bardzo otyły! \nZmień dietę, zacznij ćwiczyć! \nTwoje życie jest zagrożone!");
+        }
+        else if(bmi>31){
+            advice.setText("Jesteś otyły! \nSkontaktuj się z lekarzem! Zmień dietę, zacznij ćwiczyć!");
+        }
+
 
 
     }
